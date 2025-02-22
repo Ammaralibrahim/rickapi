3799318371cb13b94607fe259267f347baa2b9d0
Ammar <ammaryasir8088@gmail.com>
2025-02-23 00:18:57 +0300

    fianl commit

--- components/CharacterCard.tsx
+++ components/CharacterCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { Character } from '../lib/types';
 
 interface CharacterCardProps {
@@ -14,7 +14,7 @@ export function CharacterCard({ character }: CharacterCardProps) {
     }[character.status] || 'status-unknown';
 
   return (
-    <Card className="glass overflow-hidden transition-all duration-400 hover:scale-102 hover:shadow-lg glow-hover tilt w-full max-w-sm mx-auto">
+    <Card className="glass overflow-hidden transition-all duration-400 md:hover:scale-102 hover:shadow-lg glow-hover tilt w-full max-w-sm mx-auto">
       <CardHeader className="p-3">
         <CardTitle className="text-sm sm:text-base md:text-lg font-normal text-gray-200 truncate tracking-wider">
           {character.name}
@@ -24,7 +24,7 @@ export function CharacterCard({ character }: CharacterCardProps) {
         <img
           src={character.image}
           alt={character.name}
-          className="w-full h-36 sm:h-44 md:h-52 object-cover  rounded-xl mb-3 shadow-sm transition-transform duration-300 hover:scale-101"
+          className="w-full h-36 sm:h-44 md:h-52 object-cover rounded-xl mb-3 shadow-sm transition-transform duration-300 md:hover:scale-101"
         />
         <div className="space-y-2 text-xs sm:text-sm text-gray-400">
           <p>
